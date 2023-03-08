27cbac27d040cb7bc9a7d345a7c66564a071f06b
anjo22zt <anjo22zt@student.ju.se>
2023-03-08 16:47:29 +0100

    commit2

--- lunarlander.js
+++ lunarlander.js
@@ -1,39 +1,163 @@
-background(255,255,255);
-
-function flame(){
-    noStroke();
-    fill(255,180,0);
-    triangle(75,210,125,210,100,270);
-    fill(255,255,0);
-    triangle(75,210,125,210,100,250);
-}
-
-function rocket(x,y){
+ // Ufo
+  function ufo(x, y) {
+    strokeWeight(4);
+    translate(x,y);
     fill(0,0,0);
-    triangle(100,170,150,220,50,220);
+    triangle(x,y+50,x-50,y+110,x+50,y+110);
     
     fill(255,0,0);
     noStroke();
-    triangle(100,100,130,200,70,200);
-    ellipse(100,200, 59, 20);
+    triangle(x,y,x+30,y+100,x-30,y+100);
+    ellipse(x,y+100, 59, 20);
     fill(0,0,0);
     stroke(0,0,0);
-    triangle(100,100, 90,130, 110,130);
+    triangle(x,y, x-10,y+30, x+10,y+30);
     }
   
+    function flag(){
+     rect(350,350, 10,10);
+    }
+
+   function draw(){
+    flag();
+   }
     function moon(){
    fill(100,100,100);
-   ellipse(300,400, 600, 150);
+   rect(0,350, 1000,200);
+    
+  }
+  
+
+  
+  // ufo smashed
+  function crashedUfo (){
+    fill(0,0,0);
+    triangle(200,340, 125, 350, 275,350);
+    fill(255,0,0);
+    strokeWeight(4);
+    triangle(200,320,150,350,250,350);
+    fill(0,0,0);
+    triangle(200,320,175,330, 225, 330);
+  }
+  
+  
+  // Scenery
+  function scenery() {
+    push();
+    noStroke();
+  
+    background(0,0,100);
+    //stars
+    fill(255,255,255);
+    ellipse(100, 200, 5,5);
+    ellipse(25, 25, 5,5);
+    ellipse(350, 170, 5,5);
+    ellipse(250, 100, 5,5);
+    ellipse(400, 50, 5,5);
+    ellipse(200, 300, 5,5);
+    ellipse(450, 300, 5,5);
+
+  
+    // moon
+    fill(100,100,100);
+   rect(0,350, 1000,200);
+  }
+  
+  // Screens
+  // Menu screen
+  function menuScreen() {
+    scenery();
+    fill(255, 170, 50);
+    textSize(20);
+    text("CLICK TO START GAME", 20, 100);
+    textSize(24);
+    text("Use the spacebar to slow down the speed", 565, 300);
+  }
+
+  
+  // Game over screen
+  let crashedY = 410;
+  let speed = 14;
+  let crashActive = true;
+  
+  function gameOverScreen() {
+    scenery();
+    crashedUfo(100, crashedY);
+    fill(255, 0, 0);
+    textSize(38);
+    text("GAME OVER", 200, 250);
+    textSize(24);
+    text("Click to restart the game", 100, 100);
+  
+    if (crashActive) {
+      crashedY = crashedY - speed;
     }
-
-
-    function draw() {
-        moon();
-        rocket();
+    if (crashedY <= 230) {
+      crashActive = false;
     }
-
-    
-
-    
-    
- 
+  }
+  
+  // You win screen
+  function youWinScreen() {
+    scenery();
+    ufo(100, 120);
+    fill(255, 170, 50);
+    textSize(38);
+    text("YOU DID IT!", 100, 100);
+    textSize(24);
+    text("Click to restart the game", 100, 200);
+  }
+  //start
+  function mouseClicked() {
+    if (screen === 0 || screen == 3 || screen == 2) {
+      console.log("Mouse clicked");
+      ufoY = -60;
+      velocity = 1;
+      screen = 1;
+    }
+  }
+  
+  // Game screen
+  let ufoY = -60;
+  let velocity = 1.5;
+  let acceleration = 0.2;
+  let screen = 0;
+  
+  function gameScreen() {
+    scenery();
+  
+    ufoY = ufoY + velocity;
+  
+    // When spacebar is down, velocity slows down, otherwise increases
+    if (keyIsDown(32)) {
+      ufo(100, ufoY);
+      velocity = velocity - 0.2;
+    } else {
+      ufo(100, ufoY);
+      velocity += 0.2;
+    }
+    console.log(velocity);
+  
+    if (ufoY > 120) {
+      if (velocity > 2) {
+        screen = 2;
+      }
+      if (velocity < 2) {
+        screen = 3; 
+      }
+    }
+  }
+  
+  // Assigned numbers to the screens
+  function draw() {
+    if (screen === 0) {
+      menuScreen();
+    } else if (screen === 1) {
+      gameScreen();
+    } else if (screen === 2) {
+      gameOverScreen();
+    } else if (screen === 3) {
+      youWinScreen();
+    }
+  }
+  
